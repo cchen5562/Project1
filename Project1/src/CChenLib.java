265b1965db7ca36d59873ee7ecc2c8950b0c353d
BT_1N3_17 <BT_1N3_17@BT_1N3_17.bktech.local>
2017-10-05 09:20:42 -0400

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

---
+++ Project1/src/CChenLib.java
@@ -0,0 +1,12 @@
+
+public class CChenLib {
+	public static boolean isPalindrome() {
+		
+	}
+	public static void dateStr() {
+		
+	}
+	public static int sumUpTo() {
+		
+	}
+}
---
+++ Project1/src/Runner.java
@@ -0,0 +1,14 @@
+
+public class Runner {
+	public static void main(String [] args)
+	{
+		JWongLib.isFibonnaci();
+		JWongLib.multiplicationTable():
+		JWongLib.vigCipher();
+		CChenLib.isPalindrome();
+		CChenLib.dateStr();
+		CChenLib.sumUpTo();
+		System.out.println("why");
+	}
+	
+}
