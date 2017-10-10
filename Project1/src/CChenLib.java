39ee89dd8e1bc2716994d97ae0485c1de42ffe86
BT_1N3_18 <BT_1N3_18@BT_1N3_18.bktech.local>
2017-10-10 09:01:09 -0400

    smol changes

--- Project1/src/CChenLib.java
+++ Project1/src/CChenLib.java
@@ -5,14 +5,15 @@ public class CChenLib {
 				//return true;
 			//}
 		//}
+	
 		//public static void dateStr() {
 			//if("mm/dd/yyyy")
 				//System.out.println("dd - mm - yyyy");
 		//}
+	
 		public static int sumUpTo() {
 			int a;
-			while(a > 0)
-			for(int a=0; a > 0; a++) {
+			for(a = 0; a > 0; a++) {
 				
 			}
 		}
