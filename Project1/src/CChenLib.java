38a86fa842d0f7499d27e85c596b83733f3b6b24
BT_1N3_17 <BT_1N3_17@BT_1N3_17.bktech.local>
2017-10-10 09:03:57 -0400

    1212345w adsfedzs

--- Project1/src/CChenLib.java
+++ Project1/src/CChenLib.java
@@ -1,15 +1,15 @@
 
 public class CChenLib {
-	//public static boolean isPalindrome(String a, String b, String c, String d) {
-			//if(abcd == dcba) {
-				//return true;
-			//}
-		//}
+	public static boolean isPalindrome(String a, String b, String c, String d) {
+			if(abcd == dcba) {
+				return true;
+			}
+		}
 	
-		//public static void dateStr() {
-			//if("mm/dd/yyyy")
-				//System.out.println("dd - mm - yyyy");
-		//}
+		public static void dateStr() {
+			if("mm/dd/yyyy")
+				System.out.println("dd - mm - yyyy");
+		}
 	
 		public static int sumUpTo() {
 			int a;
