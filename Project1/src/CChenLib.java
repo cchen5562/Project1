cfd27ac3cb71a23e6838aa65e16432868a43f3f4
BT_1N3_18 <BT_1N3_18@BT_1N3_18.bktech.local>
2017-10-10 08:58:09 -0400

    i finally did it

--- Project1/src/CChenLib.java
+++ Project1/src/CChenLib.java
@@ -1,12 +1,19 @@
 
 public class CChenLib {
-	//public static boolean isPalindrome() {
-		
-	//}
-	//public static void dateStr() {
-		
-	//}
-	//public static int sumUpTo() {
-		
-	//}
+	//public static boolean isPalindrome(String a, String b, String c, String d) {
+			//if(abcd == dcba) {
+				//return true;
+			//}
+		//}
+		//public static void dateStr() {
+			//if("mm/dd/yyyy")
+				//System.out.println("dd - mm - yyyy");
+		//}
+		public static int sumUpTo() {
+			int a;
+			while(a > 0)
+			for(int a=0; a > 0; a++) {
+				
+			}
+		}
 }
