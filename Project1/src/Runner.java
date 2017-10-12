4778df3b695a8be4f38384308f37d3cbd60d5610
BT_1N3_17 <BT_1N3_17@BT_1N3_17.bktech.local>
2017-10-12 09:28:07 -0400

    nani the phucc

--- Project1/src/Runner.java
+++ Project1/src/Runner.java
@@ -1,15 +1,15 @@
 public class Runner {
 	public static void main(String [] args)
 	{
-		System.out.println(JWongLib.isPerfectSquare(5));
+		System.out.println(JWongLib.isPerfectSquare(4)); //prints only true :L
 		System.out.println();
-		JWongLib.vigCipher(); //not done
-		JWongLib.isFibonnaci(); //not done
+		JWongLib.vigCipher(); //not done, havent done anything
+		//JWongLib.isFibonnaci(); //not done
 		JWongLib.multiplicationTable(2,10); //this works :DDD
 		System.out.println();
-		CChenLib.isPalindrome();
+		//CChenLib.isPalindrome();
 		CChenLib.dateStr();
-		CChenLib.sumUpTo();
+		//CChenLib.sumUpTo();
 		System.out.println("None of this works and I want to die.");
 	}
 	
