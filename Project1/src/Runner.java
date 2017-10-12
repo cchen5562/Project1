d0adabc43a5d60cbbc99f491189ae5987b10adbd
BT_1N3_17 <BT_1N3_17@BT_1N3_17.bktech.local>
2017-10-12 09:22:16 -0400

    what

--- Project1/src/Runner.java
+++ Project1/src/Runner.java
@@ -1,15 +1,15 @@
 public class Runner {
 	public static void main(String [] args)
 	{
-		JWongLib.isPerfectSquare(5); //no errors but its not printing?
+		System.out.println(JWongLib.isPerfectSquare(5));
 		System.out.println();
-		JwongLib.vigCipher();
-		JWongLib.isFibonnaci();
+		JWongLib.vigCipher(); //not done
+		JWongLib.isFibonnaci(); //not done
 		JWongLib.multiplicationTable(2,10); //this works :DDD
 		System.out.println();
-		//CChenLib.isPalindrome();
-		//CChenLib.dateStr();
-		//CChenLib.sumUpTo();
+		CChenLib.isPalindrome();
+		CChenLib.dateStr();
+		CChenLib.sumUpTo();
 		System.out.println("None of this works and I want to die.");
 	}
 	
