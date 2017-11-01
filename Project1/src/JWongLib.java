b4500f423f19e6ddf7777606063ef032554eb418
BT_1N3_18 <BT_1N3_18@BT_1N3_18.bktech.local>
2017-11-01 16:10:03 -0400

    Hullo

--- Project1/src/JWongLib.java
+++ Project1/src/JWongLib.java
@@ -5,14 +5,151 @@ public class JWongLib {
 		while (y <= p) {
 			if ((y * y) == p);
 			return true;
-		}
+			}
+		
 		y++;
 		return false;
 	}
 	
 	//Vigenere Cipher chart: https://tinyurl.com/ybydczow
-	public static void vigCipher() { // shift of 4; A=E ... Z=C
-		
+	public static void vigCipher(String code) { // shift of 4; A=E ... Z=C
+		int z = 0;
+		String secret = "";
+		while (z <= code.length())
+			
+		{
+			if (code.charAt(z) == 'A')
+			{
+				secret += "E";
+				z++;
+			}
+			else if (code.charAt(z) == 'B')
+			{
+				secret += "F";
+				z++;
+			}
+			else if (code.charAt(z) == 'C')
+			{
+				secret += "G";
+				z++;
+			}
+			else if (code.charAt(z) == 'D')
+			{
+				secret += "H";
+				z++;
+			}
+			else if (code.charAt(z) == 'E')
+			{
+				secret += "I";
+				z++;
+			}
+			else if (code.charAt(z) == 'F')
+			{
+				secret += "J";
+				z++;
+			}
+			else if (code.charAt(z) == 'G')
+			{
+				secret += "K";
+				z++;
+			}
+			else if (code.charAt(z) == 'H')
+			{
+				secret += "L";
+				z++;
+			}
+			else if (code.charAt(z) == 'I')
+			{
+				secret += "M";
+				z++;
+			}
+			else if (code.charAt(z) == 'J')
+			{
+				secret += "N";
+				z++;
+			}
+			else if (code.charAt(z) == 'K')
+			{
+				secret += "O";
+				z++;
+			}
+			else if (code.charAt(z) == 'L')
+			{
+				secret += "P";
+				z++;
+			}
+			else if (code.charAt(z) == 'M')
+			{
+				secret += "Q";
+				z++;
+			}
+			else if (code.charAt(z) == 'N')
+			{
+				secret += "R";
+				z++;
+			}
+			else if (code.charAt(z) == 'O')
+			{
+				secret += "S";
+				z++;
+			}
+			else if (code.charAt(z) == 'P')
+			{
+				secret += "T";
+				z++;
+			}
+			else if (code.charAt(z) == 'Q')
+			{
+				secret += "U";
+				z++;
+			}
+			else if (code.charAt(z) == 'R')
+			{
+				secret += "V";
+				z++;
+			}
+			else if (code.charAt(z) == 'S')
+			{
+				secret += "W";
+				z++;
+			}
+			else if (code.charAt(z) == 'T')
+			{
+				secret += "X";
+				z++;
+			}
+			else if (code.charAt(z) == 'U')
+			{
+				secret += "Y";
+				z++;
+			}
+			else if (code.charAt(z) == 'V')
+			{
+				secret += "Z";
+				z++;
+			}
+			else if (code.charAt(z) == 'W')
+			{
+				secret += "A";
+				z++;
+			}
+			else if (code.charAt(z) == 'X')
+			{
+				secret += "B";
+				z++;
+			}
+			else if (code.charAt(z) == 'Y')
+			{
+				secret += "C";
+				z++;
+			}
+			else if (code.charAt(z) == 'Z')
+			{
+				secret += "D";
+				z++;
+			}
+		}
+		System.out.println(secret);
 	}
 	//public static boolean isFibonacci() {
 		
--- Project1/src/Runner.java
+++ Project1/src/Runner.java
@@ -3,12 +3,12 @@ public class Runner {
 	{
 		System.out.println(JWongLib.isPerfectSquare(4)); //prints only true :L
 		System.out.println();
-		JWongLib.vigCipher(); //not done, havent done anything
-		JWongLib.isFibonnaci(); //not done
+		JWongLib.vigCipher("LOL"); //not done, havent done anything
+		//JWongLib.isFibonnaci(); //not done
 		JWongLib.multiplicationTable(2,10); //this works :DDD
 		System.out.println();
-		CChenLib.isPalindrome(); 
-		CChenLib.dateStr(22/33/4444); 
+		//CChenLib.isPalindrome(); 
+		//CChenLib.dateStr(22/33/4444); 
 		CChenLib.sumUpTo(3);
 		System.out.println("It finally works! We are done!!!!!");
 	}
